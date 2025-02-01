847651b03cd0e5ec3489d9022e3706099801311b
anshsaluja <157537413+anshsaluja@users.noreply.github.com>
2025-01-31 23:50:15 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (41.10%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -11,15 +11,14 @@ class Solution(object):
 
         while mid<=end:
             if nums[mid] == 0:
-                nums[start], nums[mid] = nums[mid], nums [start]
+                nums[start], nums[mid] = nums[mid], nums[start]
                 start+=1
                 mid+=1
             
             elif nums[mid] == 1:
                 mid+=1
-            
+
             else:
-                nums[mid], nums[end] = nums[end] , nums[mid]
+                nums[mid], nums[end] = nums[end], nums[mid]
                 end-=1
 
-        
