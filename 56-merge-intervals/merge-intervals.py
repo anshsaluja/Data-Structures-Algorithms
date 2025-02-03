6361e816b9133a5081d9d1ed8626ec00c4f83c7f
anshsaluja <157537413+anshsaluja@users.noreply.github.com>
2025-02-03 12:05:34 -0500

    Time: 7 ms (96.54%) | Memory: 16.3 MB (34.80%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -5,24 +5,49 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
-        
         if len(intervals)<=1:
             return intervals
-
         
-        intervals.sort()
 
+        intervals.sort()
+        
         result = []
         new_interval = intervals[0]
         result.append(new_interval)
 
+
         for i in range(1,len(intervals)):
-            if intervals[i][0]<=new_interval[1]:
-                new_interval[1] = max(new_interval[1], intervals[i][1])
+            if intervals[i][0] <= new_interval[1]:
+                new_interval[1] = max(intervals[i][1], new_interval[1])
             else:
                 new_interval = intervals[i]
                 result.append(new_interval)
         
         return result
+            
+
+
+
+
+
+        
+        # if len(intervals)<=1:
+        #     return intervals
+
+        
+        # intervals.sort()
+
+        # result = []
+        # new_interval = intervals[0]
+        # result.append(new_interval)
+
+        # for i in range(1,len(intervals)):
+        #     if intervals[i][0]<=new_interval[1]:
+        #         new_interval[1] = max(new_interval[1], intervals[i][1])
+        #     else:
+        #         new_interval = intervals[i]
+        #         result.append(new_interval)
+        
+        # return result
 
         
