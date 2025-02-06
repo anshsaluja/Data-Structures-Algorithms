df561ced131c6f630e2d672d998c08569a003e51
anshsaluja <157537413+anshsaluja@users.noreply.github.com>
2025-02-05 19:59:56 -0500

    Time: 12 ms (42.75%) | Memory: 14.7 MB (5.69%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,17 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+
         majority = nums[0]
         votes = 1
 
+
         for i in range(1,len(nums)):
-            if votes == 0:
-                votes+=1
+            if votes ==  0:
                 majority = nums[i]
+                votes= 1
             elif majority == nums[i]:
                 votes+=1
             else:
                 votes-=1
 
+
         return majority
-        
+
+       
