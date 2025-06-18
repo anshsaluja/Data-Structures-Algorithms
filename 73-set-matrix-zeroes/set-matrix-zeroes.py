ed3245f5b29ed6dbebb2034dd56a0147aebac094
anshsaluja <157537413+anshsaluja@users.noreply.github.com>
2025-06-18 10:42:56 +0530

    Time: 4 ms (68.92%) | Memory: 13.1 MB (40.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,49 +1,44 @@
 class Solution(object):
     def setZeroes(self, matrix): 
 
-        first_row = False
-        first_col = False
-
         row = len(matrix)
         col = len(matrix[0])
 
+        f_row = False
+        f_col = False
+
         for i in range(row):
             for j in range(col):
-                if(matrix[i][j]==0):
+                if matrix[i][j] ==0:
                     if i == 0:
-                        first_row = True
+                        f_row = True
                     if j == 0:
-                        first_col = True
+                        f_col = True
                     matrix[i][0] = 0
                     matrix[0][j] = 0
+        
 
         for i in range(1,row):
             for j in range(1,col):
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
-                    matrix[i][j] = 0 
-
-        if first_row:
+                    matrix[i][j] =0
+        
+        if(f_row):
             for j in range(col):
-                matrix[0][j] = 0
-
-        if first_col:
+                matrix[0][j] =0
+        
+        if(f_col):
             for i in range(row):
                 matrix[i][0] = 0
 
-   
+
+                
     
-    #     for i in range(1,row):
-    #         for j in range(1,col):
-    #             if(matrix[i][0]==0 or matrix[0][j]==0):
-    #                 matrix[i][j]=0
+
+
+
         
-    #     if first_row:
-    #         for j in range(col):
-    #             matrix[0][j]=0
         
-    #     if first_col:
-    #         for i in range(row):
-    #             matrix[i][0]=0
 
 
 
