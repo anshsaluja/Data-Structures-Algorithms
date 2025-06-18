046ee3c199dbf1db3ba38b96f09e8a79590ff14f
anshsaluja <157537413+anshsaluja@users.noreply.github.com>
2025-06-18 11:44:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (89.67%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -9,63 +9,26 @@ class Solution(object):
 
         if numRows<=0:
             return result
-        
-
-        first_row = [1]
-        result.append(first_row)
+    
+        result = [[1]]
 
         if numRows<2:
             return result
 
-        
         for i in range(1,numRows):
-            prev_row = result[i-1]
-
+            prev = result[i-1]
             row = [1]
 
-            for j in range(1,len(prev_row)):
-                row.append(prev_row[j-1] + prev_row[j])
+            for j in range(1,len(prev)):
+                row.append(prev[j-1]+prev[j])
             
-
             row.append(1)
 
             result.append(row)
 
-
         return result
-         
-
         
-
-
-
-
-
-
-
-        # result = []
-
-        # if numRows == 0:
-        #     return result
-        
-        # first_row = [1]
-        # result.append(first_row)
-
-        # if numRows == 1:
-        #     return result
-        
-        # for i in range(1,numRows):
-        #     prev_row = result[i-1]
-
-        #     row = [1]
-
-        #     for j in range(1,len(prev_row)):
-        #         row.append(prev_row[j-1] + prev_row[j])
-        #     row.append(1)
-
-        #     result.append(row)
-
-        # return result
-
-        
-        
+    
+    
+    
+    
