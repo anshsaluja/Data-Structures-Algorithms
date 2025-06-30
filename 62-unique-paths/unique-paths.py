bd416e21f032d2f3cb2d1437c40f689801d53b70
anshsaluja <157537413+anshsaluja@users.noreply.github.com>
2025-06-30 17:53:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (60.80%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -6,16 +6,25 @@ class Solution(object):
         :rtype: int
         """
 
-        grid = [[0]* n for _ in range(m)]
+        grid = [[0]*n for _ in range(m)]
 
         for i in range(m):
             for j in range(n):
-                if i == 0 or j == 0:
+                if i==0 or j==0:
                     grid[i][j]=1
-                
                 else:
-                    grid[i][j] = grid[i-1][j] + grid[i][j-1]
-
-
+                    grid[i][j] = grid[i-1][j]+grid[i][j-1]
+        
         return grid[m-1][n-1]
+
+
+        # sc is O(N)
+        # dp = [1] * n
+
+        # for i in range(1,m):
+        #     for j in range(1,n):
+        #         dp[j] = dp[j] + dp[j-1]
+        
+        # return dp[-1]
+
         
