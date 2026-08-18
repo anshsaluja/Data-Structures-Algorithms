113d330a3b3d7afcdc05827ac759d62390f71f94
anshsaluja <157537413+anshsaluja@users.noreply.github.com>
2026-08-18 18:04:52 -0400

    Time: 27 ms (90.67%) | Memory: 20.4 MB (36.86%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -7,14 +7,18 @@ class Solution(object):
         result = [1]*len(nums)
 
         prefix = 1
-        for i in range(len(nums)):
-            result[i]=prefix
-            prefix = prefix * nums[i]
 
+        for i in range(len(nums)):
+            result[i] = prefix
+            prefix = prefix * nums[i]
+        
         postfix = 1
+
         for i in range(len(nums)-1,-1,-1):
-            result[i]= result[i]*postfix
+            result[i] = postfix * result[i]
             postfix = postfix * nums[i]
 
         return result
+    
+
         
