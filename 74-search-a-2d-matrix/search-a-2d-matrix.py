0886aa39fd9e49e18d9d5bd854a1d94cba38f898
anshsaluja <157537413+anshsaluja@users.noreply.github.com>
2026-08-22 19:11:02 -0400

    Time: 0 ms (100.00%) | Memory: 12.6 MB (68.35%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -6,36 +6,34 @@ class Solution(object):
         :rtype: bool
         """
 
+        rows = len(matrix)
+        cols = len(matrix[0])
 
-        if not matrix or not matrix[0]:
-            return False
-    
-        row = len(matrix)
-        col = len(matrix[0])
-
-        position = -1
-
-        for i in range(row):
-            if matrix[i][0]<=target<=matrix[i][col-1]:
-                position = i
-                break
-
-        if position == -1:
-            return False
-        
         left = 0
-        right = col-1
+        right = rows * cols - 1
+
 
         while left<=right:
-            mid = left + (right-left)//2
-            if matrix[position][mid] == target:
-                return True
-            elif matrix[position][mid]<target:
-                left = mid+1
-            else:
-                right = mid -1
-        
 
+            mid = (left+right)//2
+
+            row = mid//cols
+            col = mid%cols
+
+
+            if matrix[row][col] == target:
+                return True
+            
+            elif matrix[row][col] < target:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+        
         return False
+                
+
+
+            
 
        
